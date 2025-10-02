30884ce0ff0cd496aef659beb673ccf18b458368
debuggerboy <debuggerboy@gmail.com>
2025-10-02 21:06:53 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,88 +1,85 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"log"
-	"net/http"
+    "fmt"
+    "html/template"
+    "log"
+    "net/http"
 
-	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/apis"
-	"github.com/pocketbase/pocketbase/models"
-	"github.com/pocketbase/pocketbase/models/schema"
+    "github.com/pocketbase/pocketbase"
+    "github.com/pocketbase/pocketbase/apis"
+    "github.com/pocketbase/pocketbase/daos"
+    "github.com/pocketbase/pocketbase/models"
+    "github.com/pocketbase/pocketbase/models/schema"
 )
 
 var formTemplate = `
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Mock Data Generator</title>
+  <title>Mock Data Entry</title>
 </head>
 <body>
-	<h2>Add Mock User</h2>
-	<form method="POST" action="/mockdata">
-		<label>Name:</label><br>
-		<input type="text" name="name" required><br><br>
-		<label>Email:</label><br>
-		<input type="email" name="email" required><br><br>
-		<input type="submit" value="Add Mock User">
-	</form>
+  <h2>Add Mock User</h2>
+  <form method="POST" action="/mockdata">
+    <label>Name:</label><br>
+    <input type="text" name="name" required><br><br>
+    <label>Email:</label><br>
+    <input type="email" name="email" required><br><br>
+    <input type="submit" value="Add">
+  </form>
 </body>
 </html>
 `
 
 func main() {
-	app := pocketbase.New()
+    app := pocketbase.New()
 
-	app.OnBeforeServe().Add(func(e *pocketbase.ServeEvent) error {
-		// ensure "users" collection exists
-		collection, err := app.Dao().FindCollectionByNameOrId("users")
-		if err != nil {
-			collection = &models.Collection{
-				Name: "users",
-				Type: models.CollectionTypeBase,
-				Schema: schema.NewSchema(
-					&schema.SchemaField{
-						Name: "name",
-						Type: schema.FieldTypeText,
-					},
-					&schema.SchemaField{
-						Name: "email",
-						Type: schema.FieldTypeEmail,
-					},
-				),
-			}
-			if err := app.Dao().SaveCollection(collection); err != nil {
-				return err
-			}
-			log.Println("Created 'users' collection")
-		}
+    app.OnBeforeServe().Add(func(e *pocketbase.ServeEvent) error {
+        // Try to find “users” collection
+        coll, err := daos.New(app).FindCollectionByNameOrId("users")
+        if err != nil {
+            // If not exists, create it
+            coll = &models.Collection{
+                Name: "users",
+                Type: models.CollectionTypeBase,
+                Schema: schema.NewSchema(
+                    &schema.SchemaField{Name: "name", Type: schema.FieldTypeText},
+                    &schema.SchemaField{Name: "email", Type: schema.FieldTypeEmail},
+                ),
+            }
+            if err := daos.New(app).SaveCollection(coll); err != nil {
+                return fmt.Errorf("failed to create collection: %w", err)
+            }
+            log.Println("Created collection users")
+        }
 
-		// custom GET/POST route
-		e.Router.GET("/mockdata", func(c *apis.RequestContext) error {
-			tmpl := template.Must(template.New("form").Parse(formTemplate))
-			return tmpl.Execute(c.Response(), nil)
-		})
+        // GET endpoint to show the form
+        e.Router.GET("/mockdata", func(c *apis.RequestContext) error {
+            tmpl := template.Must(template.New("form").Parse(formTemplate))
+            return tmpl.Execute(c.Response(), nil)
+        })
 
-		e.Router.POST("/mockdata", func(c *apis.RequestContext) error {
-			name := c.Request().FormValue("name")
-			email := c.Request().FormValue("email")
+        // POST endpoint to receive form and save record
+        e.Router.POST("/mockdata", func(c *apis.RequestContext) error {
+            name := c.Request().FormValue("name")
+            email := c.Request().FormValue("email")
 
-			record := models.NewRecord(collection)
-			record.Set("name", name)
-			record.Set("email", email)
+            rec := models.NewRecord(coll)
+            rec.Set("name", name)
+            rec.Set("email", email)
 
-			if err := app.Dao().SaveRecord(record); err != nil {
-				return c.String(http.StatusInternalServerError, "❌ Failed to save record")
-			}
+            if err := daos.New(app).SaveRecord(rec); err != nil {
+                return c.String(http.StatusInternalServerError, "Error saving record: "+err.Error())
+            }
 
-			return c.String(http.StatusOK, "✅ Mock user added! <a href='/mockdata'>Add another</a>")
-		})
+            return c.String(http.StatusOK, "Mock user added! <a href=\"/mockdata\">Add more</a>")
+        })
 
-		return nil
-	})
+        return nil
+    })
 
-	if err := app.Start(); err != nil {
-		log.Fatal(err)
-	}
+    if err := app.Start(); err != nil {
+        log.Fatal(err)
+    }
 }
